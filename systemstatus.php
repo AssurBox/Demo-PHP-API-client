cc7e4c20b56753717585d7285f48d127ea042108
Samuel Beauvois <samuel.beauvois@gmail.com>
2018-08-03 16:06:00 +0200

    Oauth
    #Test Oauth > Bearer token retrieved

--- systemstatus.php
+++ systemstatus.php
@@ -18,7 +18,15 @@
 	</p>
 	
 	<?php
+	// exemple swagger	
+	//https://github.com/swagger-api/swagger-codegen/blob/master/samples/client/petstore/php/test.php
+	
+	
 require_once(__DIR__ . '/vendor/autoloader.php');
+
+// show error reporting
+ini_set('display_errors', 1);
+error_reporting(~0);
 
 include "Swagger/Client/Configuration.php";
 include "Swagger/Client/HeaderSelector.php";
@@ -34,17 +42,113 @@ print Swagger\Client\Configuration::toDebugReport();
 // to change temp folder path
 // Swagger\Client\Configuration::getDefaultConfiguration()->setTempFolderPath('/var/tmp/php/');
 // // to enable logging
-// Swagger\Client\Configuration::getDefaultConfiguration()->setDebug(true);
+ //Swagger\Client\Configuration::getDefaultConfiguration()->setDebug(true);
 // Swagger\Client\Configuration::getDefaultConfiguration()->setDebugFile('/var/tmp/php_debug.log');
 
 echo "<br/><hr/><br/>";
 
-$apiInstance = new Swagger\Client\Api\SystemCheckApi(
+
+/////
+
+// $client_id = 'HMLR4melrdXxaT5G4k3X3Q==';
+// $client_secret = 'vYy2FVXqJhaivLZqlAa6uHHgHHolPDWMhhGs52WIM5V/98pO8jRIRsKOgOC+zQzyIDKRsUj7rbwAPLNNoMb7Xg==';
+
+
+ $client_id = '73n5JXra2YtjtCsM4S9Brw==';
+ $client_secret = 'ODy8BUGdxKm5rhKp6DH/aJkpVanu1vfpr+4WjgOCEWvlmuH78c8t8HTnRAJxoKae0EhNgTpuhEczRQ68Deb0Wg==';
+
+//$redirect_uri= "http://localhost:8080/callback.php";
+//$authorization_code = $_GET['code'];
+// if(!$authorization_code){
+    // die('something went wrong!');
+// }
+$url = 'https://devslot.assurbox.net/oauth/token';
+
+
+////
+
+$myvars = 'client_id=' . $client_id . '&client_secret=' . $client_secret .'&grant_type=client_credentials';
+//echo "<br/>";
+//echo $myvars;
+
+ $data = array(
+    'client_id' => $client_id,
+    'client_secret' => $client_secret,
+    'grant_type' => 'client_credentials'
+  );
+  
+  //echo "<br/>";
+  $postdata = http_build_query($data); // avec l'encodage ca marche
+  //echo $postdata;
+   // echo "<br/>";
+ 
+//https://stackoverflow.com/questions/3080146/post-data-to-a-url-in-php
+//http://www.php.net/curl_setopt
+$ch = curl_init( $url );
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 0);
+curl_setopt( $ch, CURLOPT_HEADER, 0);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+$response = curl_exec( $ch );
+
+//var_dump($response);
+
+//echo "<br/> JSON :<br/>"; 
+
+$jsontoken=json_decode($response);
+//var_dump($jsontoken);
+
+$bearertoken = $jsontoken->access_token;
+
+// echo "<br/> Access Token :<br/>"; 
+
+// echo $bearertoken;
+
+// echo "<br/>"; 
+
+/////
+
+// test deacticate ssl check
+// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+// Swagger\Client\Configuration::getDefaultConfiguration()->setApiKeyPrefix('x-api-key', 'Bearer');
+// Swagger\Client\Configuration::getDefaultConfiguration()->setAccessToken($bearertoken);
+
+echo "<h5>Swagger config</h5>";
+print Swagger\Client\Configuration::toDebugReport();
+
+echo "<h5>system check</h5>";
+
+//https://markoivancic.from.hr/set-the-authorization-bearer-header-in-guzzle-http-client/
+$config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', $bearertoken);
+// Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+$config = Swagger\Client\Configuration::getDefaultConfiguration()->setApiKeyPrefix('api-key', 'Bearer');
+
+ // $config = $apiInstance->getConfig();
+ // $config->setApiKeyPrefix('Authorization','Bearer ');
+ // $config->setAccessToken($bearertoken);
+
+$httpClient=new GuzzleHttp\Client();
+
+$apiInstance = new Swagger\Client\Api\SystemCheckApi($httpClient,$config);
+
+//$apiInstance = new Swagger\Client\Api\SystemCheckApi(
     // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
     // This is optional, `GuzzleHttp\Client` will be used as default.
-    new GuzzleHttp\Client()
-);
+  //  new GuzzleHttp\Client());
+
+ // $config = $apiInstance->getConfig();
+ // $config->setApiKeyPrefix('Authorization','Bearer ');
+ // $config->setAccessToken($bearertoken);
+
+ //var_dump($config);
+ 
 $id = "id_example"; // string | input message
+
+//$apiInstance->getApiClient()->addDefaultHeader("Authorization", 'Bearer '.$bearertoken);
 
 try {
     $result = $apiInstance->echoGet($id);
